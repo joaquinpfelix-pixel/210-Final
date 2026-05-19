c2ec6601c5d293d77b194411946a7acae0709515
JoaquinF <joaquinpfelix@gmail.com>
2026-05-19 10:06:38 -0700

    Check 1

--- 210-Final.cpp
+++ 210-Final.cpp
@@ -1,1 +1,45 @@
 // COMSC-210 | Final | Joaquin Felix
+#include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
+
+using namespace std;
+
+const string DATA_FILE = "210-final-1-SP26.txt";
+
+int main() 
+{
+    ifstream fin(DATA_FILE);
+
+    if (!fin) 
+    {
+        cout << "ERROR: Could not openfile." << endl;
+        return 0;
+    }
+
+    map<string, int> airport_counts;
+
+    string origin;
+    string destination;
+
+    while (fin >> origin >> destination)
+    {
+        airport_counts[origin]++;
+        airport_counts[destination]++;
+    }
+
+    fin.close();
+
+    cout << "All airport traffic counts: " << endl;
+
+    for (const auto& pair : airport_counts)
+    {
+        cout << pair.first
+             << " "
+             << pair.second 
+             << endl;
+    }
+
+    return 0;
+}
